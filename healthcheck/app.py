c85dfdf6caf93f0492bcaa3a8dbbc24afa8d4b89
lquenec <lquenec@gmail.com>
2021-09-15 16:24:22 +0200

    Update app.py

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -11,6 +11,6 @@ def health():
     global calls
     dt = datetime.now()
     calls.append("from {} at {}".format(request.remote_addr, dt.strftime("%H:%M:%S")))
-    return 'OK' , 500
+    return 'OK' 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
